a7cde1cfe0670c3f80b1f3948d6c26b6622124f0
marcialj <john.marcial@utoronto.ca>
2013-03-28 16:47:01 -0300

    Update a3.js

--- a3.js
+++ a3.js
@@ -1,1 +1,4 @@
 // Javascript file
+$("body").append("<div data-role='page' id='foo'>"
+  				+ "	<div data-role='header'><h1>Foo</h1></div><!-- /header --><div data-role='content'><p>I'm first in the source order so I'm shown as the page.</p><p>View internal page called <a href='#bar'>bar</a></p></div><!-- /content --><div data-role='footer'><h4>Page Footer</h4></div><!-- /footer -->"
+);
